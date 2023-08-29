22eaf6df5c4a2409e22dc60b4e7db4f18a690df5
cristhoffer-santos <cristhoffer.santos@jbq.global>
2023-08-28 23:07:42 -0300

    feat: create dto to list user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,6 +3,7 @@ import { UserRepository } from './user.repository';
 import { CreateUserDTO } from 'src/dto/createUser.dto';
 import { UserEntity } from './user.entity';
 import { v4 as uuid } from 'uuid';
+import { ListUserDTO } from 'src/dto/listUser.dto';
 
 @Controller('/users')
 export class UserController {
@@ -18,12 +19,18 @@ export class UserController {
     userEntity.id = uuid();
 
     await this.userRepository.save(userEntity);
-    return { id: userEntity.id, message: 'User created succesfully' };
+    return {
+      user: new ListUserDTO(userEntity.id, userEntity.name),
+      message: 'User created succesfully',
+    };
   }
 
   @Get()
   async listUsers() {
-    const users = await this.userRepository.list();
-    return users;
+    const usersSaved = await this.userRepository.list();
+    const usersList = usersSaved.map(
+      (users) => new ListUserDTO(users.id, users.name),
+    );
+    return usersList;
   }
 }
